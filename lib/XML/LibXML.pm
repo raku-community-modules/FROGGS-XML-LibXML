c65d1c1e2959e956e7861f62276bcf11e35b700d
Tobias Leich <email@froggs.de>
2014-09-13 15:58:41 +0200

    use new cglobal sub exposed by NativeCall

--- lib/XML/LibXML.pm
+++ lib/XML/LibXML.pm
@@ -5,5 +5,4 @@ class XML::LibXML is XML::LibXML::Parser;
 
 use NativeCall;
 
-our sub LIBXML_VERSION returns Str is cglobal('libxml2') is symbol('xmlParserVersion') { * }
-method parser-version() { LIBXML_VERSION }
+method parser-version() { cglobal('libxml2', 'xmlParserVersion', Str) }
