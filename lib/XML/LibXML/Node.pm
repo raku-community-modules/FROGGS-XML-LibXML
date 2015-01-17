bceb5b1ef64b4f863c29e782344b372f3c3f5e9e
Tobias Leich <email@froggs.de>
2015-01-17 14:43:42 +0100

    add node.type

--- lib/XML/LibXML/Node.pm
+++ lib/XML/LibXML/Node.pm
@@ -1,6 +1,7 @@
 use v6;
 
 use XML::LibXML::CStructs :types;
+use XML::LibXML::Enums;
 
 class XML::LibXML::Node is xmlNode is repr('CStruct');
 
@@ -10,4 +11,8 @@ sub xmlNodeGetBase(xmlNode, xmlNode)  returns Str  is native('libxml2') { * }
 
 method base-uri() {
     xmlNodeGetBase(self.doc, self)
+}
+
+method type() {
+    xmlElementType(nqp::p6box_i(nqp::getattr_i(nqp::decont(self), xmlNode, '$!type')))
 }
